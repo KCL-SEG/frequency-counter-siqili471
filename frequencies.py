69ddb0b8791ad0673dc97d34b793811d177733ec
Siqi Li <1114673087@qq.com>
2023-09-30 21:15:55 +0100

    complete the frequency function

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,11 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for i in range(len(items)):
+        items[i] = str(items[i])
+    for k in range(len(items)):
+        if items[k] in frequencies:
+            frequencies[items[k]] += 1
+        else:
+            frequencies[items[k]] = 1
     return frequencies
